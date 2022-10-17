bf4fe7dbf44d91bcabfaac5aa8046370a59147b1
Weverton Santos <wever-santos@hotmail.com>
2022-10-17 11:30:04 -0300

    Colocando background, carro e ator

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
+// Imagens
+let imagemEstrada;
+let imagemCarro1;
+let imagemAtor1;
+
+// Pré carregamento da página
+function preload(){
+  imagemEstrada = loadImage("imagens/estrada.png")
+  imagemCarro1  = loadImage("imagens/carro-1.png")
+  imagemAtor1   = loadImage("imagens/ator-1.png")
+}
+
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(600, 400);
   }
   
-  function draw() {
-    background(220);
+function draw() {
+    background(imagemEstrada);
+    image(imagemCarro1, 520, 45, 60, 30)
+    image(imagemAtor1, 100, 370, 35, 25)
   }
