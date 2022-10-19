6b2d16d81e283bc70a4c865583c9bbd92255b05b
Weverton Santos <wever-santos@hotmail.com>
2022-10-19 07:47:43 -0300

    Adicionando mais carros e refinando código i++

--- carro.js
+++ carro.js
@@ -1,8 +1,8 @@
 // Variáveis dos carros
 
-let xCarros = [650, 650, 650];
-let yCarros = [45, 100, 155];
-let velocidadeCarros = [2, 2.5, 3.2];
+let xCarros = [650, 650, 650, 650, 650, 650];
+let yCarros = [45, 100, 155, 215, 267, 322];
+let velocidadeCarros = [2, 2.5, 3.5, 5, 2.3, 2.8];
 let carroComprimento = 60;
 let carroAltura = 30;
 
@@ -11,7 +11,7 @@ let carroAltura = 30;
 // Mostrando carros
 
 function mostrarCarro(){
-  for(let i = 0; i < imagemCarros.length; i += 1){
+  for(let i = 0; i < imagemCarros.length; i++){
     image(imagemCarros[i], xCarros[i], yCarros[i], carroComprimento, carroAltura)
   }
 }
@@ -19,14 +19,14 @@ function mostrarCarro(){
 // Movimentando carros
 
 function movimentadoCarro(){
-    for(let i = 0; i < imagemCarros.length; i += 1)
+    for(let i = 0; i < imagemCarros.length; i++)
     xCarros[i] -= velocidadeCarros[i];
   }
 
 // Carros para sempre
 
 function voltaCarroDoInicio(){
-  for(let i = 0; i < imagemCarros.length; i += 1){
+  for(let i = 0; i < imagemCarros.length; i++){
   if(passouTodaTela(xCarros[i])){
     xCarros[i] = 650;
     }
